74b746f7cce887a601fa13a4751724e0b49c3e7a
myhrnz1 <myhrnz@outlook.com>
2018-11-20 10:15:47 +0100

    add skeleton

---
+++ NewCalculator.java
@@ -0,0 +1,42 @@
+public class MyCalculator {
+  private static double firstNumber = 0;
+  private static double secondNumber = 0;
+  private static double answer = 0;
+  private static String operator = "-1";
+
+  private static void add(double d1, double d2){
+   //Insert your code
+  }
+  
+  private static void sub(double d1, double d2){
+    //Insert your code
+  }
+
+  private static void mult(double d1, double d2){
+    //Insert your code
+  }
+
+  private static void div(double d1, doubled d2){
+    //Insert your code
+  }
+
+  private static void enterDouble(){
+    //Insert your code
+  }
+
+  private static void enterOperator(){
+    //Insert your code
+  }
+
+  private static void printResult(){
+    //
+  }
+
+  private static void printInstructions(){
+    //
+  }
+
+  public static void main(String[] args) {
+    //Main code
+  }
+}
